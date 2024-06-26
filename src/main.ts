ce4a8bde1b9c5f673d5da32c082e90bea5500c65
jose angel <jose.albelt1817@gmail.com>
2024-06-26 09:15:12 -0500

    obtener la geolocalizavion del usuario

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+
+if( !navigator.geolocation){
+   alert('navegador no soporta la eolocation')
+   throw new Error ('Navegador no soporta la geolocalitation');
+
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
